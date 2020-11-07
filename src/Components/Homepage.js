1eb782daaa057ecb0a0e07fd3fb69086a12fe0ba
denisechan1 <chanxdenise@gmail.com>
2020-11-07 14:11:11 -0800

    Added title and background

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -1,13 +1,21 @@
 import React from 'react'
 import firebase from "firebase"
 import "../firebase.js"
+require('typeface-rock-salt') 
 
 function Homepage() {
-    var user = firebase.auth().currentUser.displayName;
+    //var user = firebase.auth().currentUser.displayName;
+    const mystyle = {
+        color: "#204051",
+        backgroundColor: "#E7DFD5",
+        padding: "36px",
+        fontFamily: "Rock Salt",
+        textAlign: "center"
+      };
     return (
-        <div>
-            <p>{user}</p>
-        </div>
+        <h1 style= {mystyle} >
+            <p>Linked</p>
+        </h1>
     )
 }
 export default Homepage
