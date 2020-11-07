078e1fd98b8c84b8900c6b54ebe1f22c8267a3ec
sbala025 <sbala025@ucr.edu>
2020-11-07 14:45:40 -0800

    temp

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import firebase from "firebase"
 import "../firebase.js"
+import "../Styles/App.css"
 
 function Homepage() {
     var user = firebase.auth().currentUser.displayName;
     return (
+        <>
+        <body>
         <div>
+            <h1 style={{"textAlign":"center"}}>Linked</h1>
             <p>{user}</p>
         </div>
+        </body>
+        </>
     )
 }
 export default Homepage
