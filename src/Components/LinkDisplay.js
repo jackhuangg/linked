74ef333f401d049cabcc490550fad832b6d34a77
Sanchit Goel <sgoel008@ucr.edu>
2020-11-07 19:45:43 -0800

    Open links in new tab

--- src/Components/LinkDisplay.js
+++ src/Components/LinkDisplay.js
@@ -6,7 +6,7 @@ function LinkDisplay({ link, onDelete, isHref }) {
     return (
     <Row>
         <Col>
-            { isHref ? <a href={"http://" +  link}>{link}</a> : <p>{link}</p> }
+            { isHref ? <a href={"http://" +  link} target="_blank">{link}</a> : <p>{link}</p> }
         </Col>
         <Col>
             <button style={{ position: "absolute", right: "5%", top: "25%" }}
